e023e58b198eb1cfa126fbcf0a32c7f1535c3885
Naufal <juznauf@gmail.com>
2021-06-20 17:37:00 +0800

    refactor testcases.java
    for homework1 testcases
    removed redundant lines

--- homework/homework-1/TestCases.java
+++ homework/homework-1/TestCases.java
@@ -8,23 +8,13 @@ public class TestCases {
         String[] newArgs = new String[2];
         newArgs[0] = "grades.csv";
         
+        String[] testCases = {"10", "20","101"};
         
-        // test case with bins = 10
-        System.out.println("Test case with bins = 10...");
-        newArgs[1] = "10";
-        Histogram.main(newArgs);
-        System.out.println();
-        
-        // test case with bins = 20
-        System.out.println("Test case with bins = 20...");
-        newArgs[1] = "20";
-        Histogram.main(newArgs);
-        System.out.println();
-        
-        // test case with bins = 101
-        System.out.println("Test case with bins = 101...");
-        newArgs[1] = "101";
-        Histogram.main(newArgs);
-        System.out.println();
+        for (String bin : testCases) {
+            System.out.printf("Test case with bins = %s...%n",bin);
+            newArgs[1] = bin;
+            Histogram.main(newArgs);
+            System.out.println();
+        }
     }
 }
